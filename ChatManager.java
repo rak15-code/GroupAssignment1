390b9858bf5c2fd4e842c30970982d4226417d2b
farznr <nurulfarzana114@gmail.com>
2025-05-19 10:58:15 +0800

    Update ChatManager.java

--- ChatManager.java
+++ ChatManager.java
@@ -1,3 +1,13 @@
+// Group Tutorial 13
+// Group Members:
+// Student 1: [Name] – Singly Linked List
+// Student 2: [Name] – Indexed List
+// Student 3: [Name] – Doubly Linked List with Cursor
+// Student 4: Nurul Farzana Binti Anuar – Undo/Redo with Stack
+// Student 5: [Name] – Testing
+// Student X: [Name] – Absent
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -70,4 +80,47 @@ public class ChatManager {
             System.out.println("null");
 
         }
+
+//=====Student 4=====
+//Nurul Farzana Binti Anuar - Undo/Redo with Stack
+public class UndoRedoManager {
+    private final Stack <String> undo = new Stack<>();
+    private final Stack <String> redo = new Stack<>();
+    
+    public void perform(String action){
+        System.out.println("Perform: " + action);
+        
+        undo.push(action);
+        redo.clear();
+    }
+    
+    public String Undo(){
+        if(!undo.isEmpty()){
+            String action = undo.pop();
+            redo.push(action);
+            return action;
+        }else{
+            return null;
+        }
+     
+    }
+    
+    public String Redo(){
+        if(!redo.isEmpty()){
+            String action = redo.pop();
+            undo.push(action);
+            return action;
+        }else{
+           return null;
+        }
+     
+    }
+    
+    public void printStacks(){
+        System.out.println("Undo Stack: " + undo);
+        System.out.println("redo Stack: " + redo);
+    }
+    
+    
+}
     }
